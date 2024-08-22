801263f899883e3c5d263f87030fce51c5dada98
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-08-22 11:05:58 +0200

    add data for 2015

--- main.py
+++ main.py
@@ -23,22 +23,20 @@ USA = geopandas.read_file(geoplot.datasets.get_path('contiguous_usa'))
 # i = USA[(USA.state == 'North Carolina')].index
 # USA.drop(i)
 
-USA.boundary.drop(30)
-
 # Set the map projection
 proj = geoplot.crs.AlbersEqualArea(central_longitude=-98, central_latitude=39.5)
 
 # Setup the Voronoi axes; this creates the Voronoi regions
-ax = geoplot.voronoi(gdf,  # Define the GeoPandas DataFrame
+ax = geoplot.voronoi(gdf, # Define the GeoPandas DataFrame
                      figsize=(20,14), # Define resolution of figure
-                     hue='values',  # df column used to color regions
-                     clip=USA,  # Define the voronoi clipping (map edge)
-                     projection=proj,  # Define the Projection
-                     cmap='Greys',  # color set
-                     # k=None,  # No. of discretized buckets to create
+                     hue='values', # df column used to color regions
+                     clip=USA,  # Define the voronoi clipping (map edge) -TODO VA source here?
+                     projection=proj, # Define the Projection
+                     cmap='Greys', # color set
+                     # k=None, # No. of discretized buckets to create
                      legend=False, # Dont create a legend
-                     edgecolor='#000000',  # Color of the voronoi boundaries
-                     linewidth=0.5  # width of the voronoi boundary lines
+                     edgecolor='#000000', # Color of the voronoi boundaries
+                     linewidth=0.5 # width of the voronoi boundary lines
                     )
 
 # Render the plot with a base map
@@ -46,7 +44,7 @@ geoplot.polyplot(USA,  # Base Map
                  ax=ax,  # Axis attribute we created above
                  extent=[-120, 25, -73, 49], # extent=USA.total_bounds,  # Set plotting boundaries to base map boundaries
                  edgecolor='black',  # Color of base map's edges
-                 linewidth=0,  # Width of base map's edge lines
+                 linewidth=3,  # Width of base map's edge lines
                  zorder=1  # Plot base map edges above the voronoi regions
                  )
 
