7e647b69fbaf36dd37a92e0e0fec9beff54ea762
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-08-25 09:49:49 +0200

    fully compartmentalize the plat function

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-# Import pandas, and matplotlib
+# Import pandas, numpy, and matplotlib
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 # Import Geopandas modules
 import geopandas
 import geoplot
 # Import shapely to convert string lat-longs to Point objects
 from shapely.geometry import Point
+# Import date and time formatters
+from time import strptime
+import datetime
 
-def plot_voronoi(i):
+
+def plot_voronoi(i, geodf, basemap, projection):
     # Setup the Voronoi axes; this creates the Voronoi regions
     ax = geoplot.voronoi(
-        gdf, # Define the GeoPandas DataFrame
+        geodf, # Define the GeoPandas DataFrame
         figsize=(20,14), # Define resolution of figure
         hue='values', # df column used to color regions
-        clip=USA,  # Define the voronoi clipping (map edge) -TODO VA source here?
-        projection=proj, # Define the Projection
+        clip=basemap,  # Define the voronoi clipping (map edge) -TODO VA source here?
+        projection=projection, # Define the Projection
         cmap='Greys', # color set
         # k=None, # No. of discretized buckets to create
         legend=False, # Dont create a legend
@@ -23,7 +28,7 @@ def plot_voronoi(i):
         )
 
     # Render the plot with a base map
-    geoplot.polyplot(USA,  # Base Map
+    geoplot.polyplot(basemap,  # Base Map
                     ax=ax,  # Axis attribute we created above
                     extent=[-120, 25, -73, 49], # extent=USA.total_bounds,  # Set plotting boundaries to base map boundaries
                     edgecolor='black',  # Color of base map's edges
@@ -35,37 +40,38 @@ def plot_voronoi(i):
 
 # CSV into DataFrame
 # df = pd.read_table("trial.csv", delimiter =",")
-df = pd.read_table("nps.csv", delimiter =",")
-# df['name'] = df['name'].astype("string") # which will by default set the length to the max len it encounters
-# print(df.tail())
+# df = pd.read_table("nps.csv", delimiter =",")
+df = pd.read_table("nps_list.csv", delimiter =",")
 
-
-# gdf.set_geometry('geometry', inplace=True)
+# Sort the dataframe by date
+# df = df.sort_values(by=['date'])
 
 # Import USA data for region clipping
 USA = geopandas.read_file(geoplot.datasets.get_path('contiguous_usa'))
-# i = USA[(USA.state == 'North Carolina')].index
-# USA.drop(i)
 
 # Set the map projection
 proj = geoplot.crs.AlbersEqualArea(central_longitude=-98, central_latitude=39.5)
 
+# Iterate through sites visited
 counter = 0
 for index, row in df.iterrows():
-    if row['date'] != "NAN":
+    # if not np.isnan(row['date']):
+    if row['date'] != 0:
+        print(row['date'], type(row['date']))
+        # Set current region to active
         df.at[index,'values'] = 1
 
-        print(df['name'], df['values'])
-
-        # Assumes data stored in pandas DataFrame df
+        # Convert df to gdf (GeoPandas DataFrame)
         geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
-        # print(geometry)
-        df['geometry'] = geometry
-        gdf = geopandas.GeoDataFrame(df, geometry=df.geometry)
+        # df['geometry'] = geometry
+        gdf = geopandas.GeoDataFrame(df, geometry=geometry)
 
+        # Plot the current map state
         counter += 1
-        plot_voronoi(counter)
+        plot_voronoi(counter, geodf=gdf, basemap=USA, projection=proj)
 
+        # Set current region to inactive
         df.at[index,'values'] = 0.75
 
-print(gdf)
+counter += 1
+plot_voronoi(counter, geodf=gdf, basemap=USA, projection=proj)
