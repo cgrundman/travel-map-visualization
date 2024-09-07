204a8b537b64d287b94eff8ececde29c5708d3f3
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-09-07 16:00:59 +0200

    generalize codebase (main.py)

--- main.py
+++ main.py
@@ -9,33 +9,48 @@ from shapely.geometry import Point
 
 
 # Set global variables, directories for map creation and site locations
-LOCATION_CSV = "nps_list.csv"
-GEO_DATA_DIR = "cb_2018_us_nation_5m/cb_2018_us_nation_5m.shp"
+
+# National Park Data
+# MAP_NAME = "national_parks"
+# EXTENT=[-120, 25, -73, 49]
+# CENTRAL_LONGITUDE=-98
+# CENTRAL_LATITUDE=39.5
+# LOCATION_CSV = "National_Parks/nps_list.csv"
+# GEO_DATA_DIR = "National_Parks/cb_2018_us_nation_5m/cb_2018_us_nation_5m.shp"
+
+# Germany Sites
+MAP_NAME = "germany_sites"
+EXTENT=[6, 47, 15, 55]
+CENTRAL_LONGITUDE=10.5
+CENTRAL_LATITUDE=51
+LOCATION_CSV = "Sehenswuerdigkeiten/sehenswuerdigkeiten.csv"
+GEO_DATA_DIR = "Sehenswuerdigkeiten/Germany_Boundary/germany_Germany_Country_Boundary.shp"
 
 
 def plot_voronoi(counter, geodf, basemap, projection, site):
     # Setup the Voronoi axes; this creates the Voronoi regions
     ax = geoplot.voronoi(
         geodf, # Define the GeoPandas DataFrame
-        figsize=(20,14), # Define resolution of figure
+        figsize=(14,18), # Define resolution of figure
         hue='values', # df column used to color regions
-        clip=basemap,  # Define the voronoi clipping (map edge) -TODO VA source here?
+        clip=basemap,  # Define the voronoi clipping (map edge)
         projection=projection, # Define the Projection
         cmap='YlGn', # color set
         # k=None, # No. of discretized buckets to create
         legend=False, # Dont create a legend
         edgecolor='#000000', # Color of the voronoi boundaries
         linewidth=0.5 # width of the voronoi boundary lines
-        )
+    )
 
     # Render the plot with a base map
-    geoplot.polyplot(basemap,  # Base Map
-                    ax=ax,  # Axis attribute we created above
-                    extent=[-120, 25, -73, 49], # extent=USA.total_bounds,  # Set plotting boundaries to base map boundaries
-                    edgecolor='black',  # Color of base map's edges
-                    linewidth=3,  # Width of base map's edge lines
-                    zorder=1  # Plot base map edges above the voronoi regions
-                    )
+    geoplot.polyplot(
+        basemap,  # Base Map
+        ax=ax,  # Axis attribute we created above
+        extent=EXTENT, # Set plotting boundaries to base map boundaries
+        edgecolor='black',  # Color of base map's edges
+        linewidth=3,  # Width of base map's edge lines
+        zorder=1  # Plot base map edges above the voronoi regions
+    )
     
     # Format date
     date_list = list(map(str, str(site['date'])))
@@ -48,7 +63,7 @@ def plot_voronoi(counter, geodf, basemap, projection, site):
     day = ''.join(day)
     # year = ''.join(date_list[:4])
     plt.title(f'{site['name']} {site['designation']}, {month}/{day}/{year}', fontsize=36, loc='right')
-    plt.savefig(f'plots/nps_segments_{counter}.png')
+    plt.savefig(f'plots/{MAP_NAME}_{counter}.png')
     plt.close()
 
 # CSV into DataFrame
@@ -61,7 +76,10 @@ df = df.sort_values(by=['date'])
 base_map = geopandas.read_file(GEO_DATA_DIR)
 
 # Set the map projection
-proj = geoplot.crs.AlbersEqualArea(central_longitude=-98, central_latitude=39.5)
+proj = geoplot.crs.AlbersEqualArea(
+    central_longitude=CENTRAL_LONGITUDE, 
+    central_latitude=CENTRAL_LATITUDE
+)
 
 # Iterate through sites visited
 counter = 0
