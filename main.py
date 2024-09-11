6c357dc49e1b8994e26f2eb1d80285b9a4326c17
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-09-11 13:53:24 +0200

    generalize map coloring, figure size

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Import pandas, numpy, and matplotlib
 import pandas as pd
 from matplotlib import pyplot as plt
-import math
 # Import Geopandas modules
 import geopandas
 import geoplot
@@ -12,27 +11,30 @@ from shapely.geometry import Point
 # Set global variables, directories for map creation and site locations
 
 # National Park Data
-# MAP_NAME = "national_parks"
-# EXTENT=[-120, 25, -73, 49]
-# CENTRAL_LONGITUDE=-98
-# CENTRAL_LATITUDE=39.5
-# LOCATION_CSV = "National_Parks/nps_list.csv"
-# GEO_DATA_DIR = "National_Parks/cb_2018_us_nation_5m/cb_2018_us_nation_5m.shp"
+MAP_NAME = "national_parks"
+EXTENT=[-120, 25, -73, 49]
+CENTRAL_LONGITUDE=-98
+CENTRAL_LATITUDE=39.5
+LOCATION_CSV = "National_Parks/nps_list.csv"
+GEO_DATA_DIR = "National_Parks/cb_2018_us_nation_5m/cb_2018_us_nation_5m.shp"
+COLOR_VALUES = [0.56,.21,0.26] # [unvisited,visited-active,visited-inactive]
+FIG_SIZE = (20,14)
 
 # Germany Sites
-MAP_NAME = "germany_sites"
-EXTENT=[6, 47, 15, 55]
-CENTRAL_LONGITUDE=10.5
-CENTRAL_LATITUDE=51
-LOCATION_CSV = "Sehenswuerdigkeiten/sehenswuerdigkeiten.csv"
-GEO_DATA_DIR = "Sehenswuerdigkeiten/Germany_Boundary/germany_Germany_Country_Boundary.shp"
-
+# MAP_NAME = "germany_sites"
+# EXTENT=[6, 47, 15, 55]
+# CENTRAL_LONGITUDE=10.5
+# CENTRAL_LATITUDE=51
+# LOCATION_CSV = "Sehenswuerdigkeiten/sehenswuerdigkeiten.csv"
+# GEO_DATA_DIR = "Sehenswuerdigkeiten/Germany_Boundary/germany_Germany_Country_Boundary.shp"
+# COLOR_VALUES = [0.51,.61,0.66] # [unvisited,visited-active,visited-inactive]
+# FIG_SIZE = (14,18)
 
 def plot_voronoi(counter, geodf, basemap, projection, site):
     # Setup the Voronoi axes; this creates the Voronoi regions
     ax = geoplot.voronoi(
         geodf, # Define the GeoPandas DataFrame
-        figsize=(14,18), # Define resolution of figure
+        figsize=FIG_SIZE, # Define resolution of figure
         hue='values', # df column used to color regions
         clip=basemap,  # Define the voronoi clipping (map edge)
         projection=projection, # Define the Projection
@@ -63,10 +65,12 @@ def plot_voronoi(counter, geodf, basemap, projection, site):
     day = map(str, day_list)
     day = ''.join(day)
     # year = ''.join(date_list[:4])
-    if math.isnan(site['designation']):
+    # if math.isnan(site['designation']):
+    if type(site['designation']) == type(0.0):
         plt.title(f'{site['name']}, {month}/{day}/{year}', fontsize=36, loc='right')
-    else:
+    elif type(site['designation']) == type("string"):
         plt.title(f'{site['name']} {site['designation']}, {month}/{day}/{year}', fontsize=36, loc='right')
+    print(f"Saving plot: {MAP_NAME}_{counter}")
     plt.savefig(f'plots/{MAP_NAME}_{counter}.png')
     plt.close()
 
@@ -75,7 +79,7 @@ df = pd.read_table(LOCATION_CSV, delimiter =",")
 
 # Iterate through sites visited
 for index, row in df.iterrows():
-    df.at[index,'values'] = 0.51
+    df.at[index,'values'] = COLOR_VALUES[0]
 
 # Add min and max points
 max_row = [len(df),"Max Point","not a point",34.593218347568765,69.17071209340384,1.0,0]
@@ -101,7 +105,7 @@ for index, row in df.iterrows():
     # if not np.isnan(row['date']):
     if row['date'] != 0:
         # Set current region to active
-        df.at[index,'values'] = .61
+        df.at[index,'values'] = COLOR_VALUES[1]
 
         # Convert df to gdf (GeoPandas DataFrame)
         geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
@@ -110,11 +114,11 @@ for index, row in df.iterrows():
 
         # Plot the current map state
         counter += 1
-        # plot_voronoi(counter, geodf=gdf, basemap=base_map, projection=proj, site=row)
+        plot_voronoi(counter, geodf=gdf, basemap=base_map, projection=proj, site=row)
         
         # Set current region to inactive
-        df.at[index,'values'] = 0.66
+        df.at[index,'values'] = COLOR_VALUES[2]
 
 # Toubleshooting Map
 # print(counter)
-plot_voronoi(counter, geodf=gdf, basemap=base_map, projection=proj, site=row)
+# plot_voronoi(counter, geodf=gdf, basemap=base_map, projection=proj, site=row)
