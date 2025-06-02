ee099dd3da35c3e366522cb1d40c7c5a7f2a18a1
cgrundman <christian.grundman@gmail.com>
2025-06-02 10:54:39 +0200

    add documentation for odering csv file

--- order_csv.py
+++ order_csv.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
+# Paths for CSV files
+OLD_CSV = "Sehenswuerdigkeiten/sehenswuerdigkeiten.csv" # old path
+NEW_CSV = OLD_CSV.replace(".csv", "_new.csv") # new path
 
-OLD_CSV = "Sehenswuerdigkeiten/sehenswuerdigkeiten.csv"
-NEW_CSV = OLD_CSV.replace(".csv", "_new.csv")
+# Read the CSV file using pandas
+df = pd.read_table(OLD_CSV, delimiter=",")
 
-df = pd.read_table(OLD_CSV, delimiter =",")
-
+# Sort the DataFrame by the 'latitude' column in descending order
 df = df.sort_values('latitude', ascending=False)
 
+# Remove the 'number' column from the DataFrame
 df = df.drop(columns='number')
 
+# Reset the DataFrame index and drop the old index column
 df = df.reset_index(drop=True)
 
-df.to_csv(NEW_CSV, sep=",")
+# Write the cleaned and sorted DataFrame to a new CSV file
+df.to_csv(NEW_CSV, sep=",", index=False)
