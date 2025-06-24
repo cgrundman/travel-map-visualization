e4f96777df2ad3a07a7cc9dd0a9a6dd923fb770d
cgrundman <christian.grundman@gmail.com>
2025-06-24 10:29:40 +0200

    create inital file for custom color map

---
+++ colormap.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import numpy as np
+
+color_1 = (132, 60, 57)
+color_2 = (214, 97, 107)
+
+# Create a custom linear colormap
+custom_cmap = LinearSegmentedColormap.from_list("custom_map", [tuple([x / 255 for x in (color_1)]), tuple([x / 255 for x in (color_2)])])
+
+# Function to get color at position between 0 and 1
+def get_color(value):
+    return custom_cmap(value)
+
+# Example usage
+value = 0.25
+color_at_value = get_color(value)
+print(f"Color at {value}: {color_at_value}")
+
+gradient = np.linspace(0, 1, 256).reshape(1, -1)
+plt.imshow(gradient, aspect='auto', cmap=custom_cmap)
+plt.title("Custom Color Map")
+plt.axis('off')
+plt.show()
