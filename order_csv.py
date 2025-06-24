850f3fddfd169fc1df5d675b3802dfad98784d2c
cgrundman <christian.grundman@gmail.com>
2025-06-24 20:25:40 +0200

    reorder csv files

--- order_csv.py
+++ order_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Paths for CSV files
-OLD_CSV = "National_Parks/nps_list.csv" # old path
+OLD_CSV = "de/locations.csv" # old path
 NEW_CSV = OLD_CSV.replace(".csv", "_new.csv") # new path
 
 # Read the CSV file using pandas
@@ -10,8 +10,8 @@ df = pd.read_table(OLD_CSV, delimiter=",")
 # Sort the DataFrame by the 'latitude' column in descending order
 df = df.sort_values('latitude', ascending=False)
 
-# Remove the 'number' column from the DataFrame
-df = df.drop(columns='number')
+# Resort the columns
+df = df[['submap','latitude','longitude','date','name']]
 
 # Reset the DataFrame index and drop the old index column
 df = df.reset_index(drop=True)
