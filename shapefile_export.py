0084db438d45c78f625d6f80645ad8e42f01b3ec
cgrundman <christian.grundman@gmail.com>
2025-07-09 13:43:10 +0200

    add submaps for the states

--- shapefile_export.py
+++ shapefile_export.py
@@ -2,19 +2,19 @@ import geopandas as gpd
 import os
 
 # Load shapefile
-gdf = gpd.read_file("eu/submaps/Kosovo/geoBoundaries-XKX-ADM0_simplified.shp")
+gdf = gpd.read_file("us/submaps/geoBoundaries-USA-ADM1-all/geoBoundaries-USA-ADM1_simplified.shp")
 
 # Create a directory for output shapefiles
-output_dir = "eu/submaps"
+output_dir = "us/submaps"
 os.makedirs(output_dir, exist_ok=True)
 
-# # Group by state name and export each as its own shapefile
-# for state_name in gdf["shapeISO"].unique():
-#     state_gdf = gdf[gdf["shapeISO"] == state_name]
-#     filename = os.path.join(output_dir, f"{state_name.replace(' ', '_').replace('DE-', '')}.shp")
-#     state_gdf.to_file(filename)
-#     print(f"Exported: {filename}")
+# Group by state name and export each as its own shapefile
+for state_name in gdf["shapeISO"].unique():
+    state_gdf = gdf[gdf["shapeISO"] == state_name]
+    filename = os.path.join(output_dir, f"{state_name.replace(' ', '_').replace('US-', '')}.shp")
+    state_gdf.to_file(filename)
+    print(f"Exported: {filename}")
 
-filename = os.path.join(output_dir, "XK.shp")
-gdf.to_file(filename)
-print(f"Exported: {filename}")
+#filename = os.path.join(output_dir, ".shp")
+#gdf.to_file(filename)
+#print(f"Exported: {filename}")
