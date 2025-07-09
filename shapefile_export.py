cf8fda4ec5f4a0542977ffd932c70da2496ee3af
cgrundman <christian.grundman@gmail.com>
2025-07-09 08:05:53 +0200

    add latvia, lithuania, macedonia, moldova, montenegro

--- shapefile_export.py
+++ shapefile_export.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import os
 
 # Load shapefile
-gdf = gpd.read_file("eu/submaps/hungary/geoBoundaries-HUN-ADM0_simplified.shp")
+gdf = gpd.read_file("eu/submaps/Latvia/geoBoundaries-LVA-ADM0_simplified.shp")
 
 # Create a directory for output shapefiles
 output_dir = "eu/submaps"
@@ -15,6 +15,6 @@ os.makedirs(output_dir, exist_ok=True)
 #     state_gdf.to_file(filename)
 #     print(f"Exported: {filename}")
 
-filename = os.path.join(output_dir, "HU.shp")
+filename = os.path.join(output_dir, "LV.shp")
 gdf.to_file(filename)
 print(f"Exported: {filename}")
