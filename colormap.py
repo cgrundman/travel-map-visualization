3fb12d53dd44963cf7b1bb68fd571af94c4c3081
cgrundman <christian.grundman@gmail.com>
2025-07-11 00:29:50 +0200

    add metadata to colormap generator

--- colormap.py
+++ colormap.py
@@ -1,12 +1,17 @@
 # import matplotlib.pyplot as plt
+import json
 from matplotlib.colors import LinearSegmentedColormap
 # import numpy as np
 
-def custom_cmap(ratio):
+def custom_cmap(path, ratio):
+
+    # Load the JSON Meta-Data
+    with open(path + '/meta_data.json', 'r') as f:
+        meta_data = json.load(f)
+    
     # Color Ranges
-    color_1 = (132, 60, 57)
-    #color_2 = (214, 97, 107)
-    color_2 = (189, 158, 57)
+    color_1 = hex_to_rgb(meta_data["Colors"]["map_dark"])
+    color_2 = hex_to_rgb(meta_data["Colors"]["map_light"])
 
     # Create a custom linear colormap
     cmap = LinearSegmentedColormap.from_list("custom_map", [tuple([x / 255 for x in (color_1)]), tuple([x / 255 for x in (color_2)])])
@@ -14,6 +19,10 @@ def custom_cmap(ratio):
     color = cmap(ratio)
 
     return color
+
+def hex_to_rgb(hex_code):
+    hex_code = hex_code.lstrip('#')
+    return tuple(int(hex_code[i:i+2], 16) for i in (0, 2, 4))
 
 # Function to get color at position between 0 and 1
 #def get_color(value):
