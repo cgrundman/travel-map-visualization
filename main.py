264353176a8eb84f856c4f42dde7276d0f6c8721
cgrundman <christian.grundman@gmail.com>
2025-10-27 11:25:06 +0100

    update the large plots

--- main.py
+++ main.py
@@ -1,63 +1,63 @@
-import pandas as pd
-import datetime
+#import pandas as pd
+#import datetime
 
-from metadata.meta_loader import MetaLoader
-from data.points_loader import PointsLoader
-from data.submaps_loader import SubmapsLoader
-from plotting.plotting_manager import PlotManager
+#from metadata.meta_loader import MetaLoader
+#from data.points_loader import PointsLoader
+#from data.submaps_loader import SubmapsLoader
+#from plotting.plotting_manager import PlotManager
 from plotting.gif_generator import GifGenerator
-from utils.file_utils import ensure_directory_exists
+#from utils.file_utils import ensure_directory_exists
 
 
 # Set global variables, directories for map creation and site locations
-SCALE = 4
+SCALE = 3
 
 # US National Park Global Variables
-#PATH = "us"
+PATH = "us"
 
 # Germany Global Variables
-PATH = "de"
+#PATH = "de"
 
 # Europe Global Variables
 #PATH = "eu"
 
 # Ensure output folders exist
-ensure_directory_exists("plots/temp")
-ensure_directory_exists("plots")
-ensure_directory_exists("gifs")
+#ensure_directory_exists("plots/temp")
+#ensure_directory_exists("plots")
+#ensure_directory_exists("gifs")
 
 # Load the JSON Meta-Data, Points data, and map data
-meta_data = MetaLoader(PATH).load()
-points_gdf = PointsLoader(PATH).load()
-submaps = SubmapsLoader(PATH).load()
+#meta_data = MetaLoader(PATH).load()
+#points_gdf = PointsLoader(PATH).load()
+#submaps = SubmapsLoader(PATH).load()
 
 # Create a sorted values df
-points_sorted = points_gdf.sort_values('date')
+#points_sorted = points_gdf.sort_values('date')
 
 # Generate plots
-old_date = None
-plot_manager = PlotManager(
-    points_gdf=points_gdf,
-    submaps=submaps,
-    meta_data=meta_data,
-    path=PATH,
-    scale=SCALE
-)
+#old_date = None
+#plot_manager = PlotManager(
+#    points_gdf=points_gdf,
+#    submaps=submaps,
+#    meta_data=meta_data,
+#    path=PATH,
+#    scale=SCALE
+#)
 
 # Add first plot
-current_date = points_sorted['date'].min() - datetime.timedelta(days=1)
-plot_manager.generate_plot(current_date, points_sorted.iloc[0], copy=True)
+#current_date = points_sorted['date'].min() - datetime.timedelta(days=1)
+#plot_manager.generate_plot(current_date, points_sorted.iloc[0], copy=True)
 
 # Plot all dates
-for _, row in points_sorted.iterrows():
-    current_date = row['date']
-    if pd.notna(current_date) and current_date != old_date:
-        plot_manager.generate_plot(current_date, row)
-        old_date = current_date
+#for _, row in points_sorted.iterrows():
+#    current_date = row['date']
+#    if pd.notna(current_date) and current_date != old_date:
+#        plot_manager.generate_plot(current_date, row)
+#        old_date = current_date
 
 # Create Last Plot
-old_date += datetime.timedelta(days=1)
-plot_manager.generate_plot(old_date, row, copy=True)
+#old_date += datetime.timedelta(days=1)
+#plot_manager.generate_plot(old_date, row, copy=True)
 
 # Create gif from produced plots
 gif = GifGenerator(
