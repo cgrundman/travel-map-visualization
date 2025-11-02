3caeeeb18deccc73bde211804f6d8a4174c2053b
cgrundman <christian.grundman@gmail.com>
2025-11-02 16:10:34 +0100

    update flag conversion scale

--- svg_conversion.py
+++ svg_conversion.py
@@ -6,4 +6,4 @@ PATH = "de"
 svg_files = [f for f in os.listdir(f"{PATH}/submaps") if f.lower().endswith(".svg")]
 for file in svg_files:
     #print(file[:-4])
-    cairosvg.svg2png(url=f"{PATH}/submaps/{file}", write_to=f"{PATH}/submaps/{file[:-4]}.png")
+    cairosvg.svg2png(url=f"{PATH}/submaps/{file}", write_to=f"{PATH}/submaps/{file[:-4]}.png", output_height=200)
