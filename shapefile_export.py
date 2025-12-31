f086b40f6c54563ef60b7290fb67a87188b2be09
cgrundman <christian.grundman@gmail.com>
2025-12-31 13:58:04 +0100

    add ir files

--- shapefile_export.py
+++ shapefile_export.py
@@ -2,15 +2,19 @@ import geopandas as gpd
 import os
 
 # Load shapefile
-gdf = gpd.read_file("eu/submaps/geoBoundaries-SRB-ADM0-all/geoBoundaries-SRB-ADM0_simplified.shp")
+gdf = gpd.read_file("ir/submaps/geoBoundaries-IRN-ADM1_simplified.shp")
 
 # Create a directory for output shapefiles
-output_dir = "eu/submaps"
+output_dir = "ir/submaps"
 os.makedirs(output_dir, exist_ok=True)
 
+#gdf.xs(30)['shapeISO'] = 'IR-07'
+
 # Group by state name and export each as its own shapefile
-for state_name in gdf["shapeISO"].unique():
-    state_gdf = gdf[gdf["shapeISO"] == state_name]
+for state_name in gdf["shapeName"].unique():
+    if not gdf["shapeName"]:
+        gdf["shapeName"]='IR-07'
+    state_gdf = gdf[gdf["shapeName"] == state_name]
     filename = os.path.join(output_dir, f"{state_name.replace(' ', '_').replace('US-', '')}.shp")
     state_gdf.to_file(filename)
     print(f"Exported: {filename}")
