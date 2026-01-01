ff4aa42c114cc2ff3c79c2a7b67ab0a7a1147be9
cgrundman <christian.grundman@gmail.com>
2026-01-01 14:14:59 +0100

    create submaps for iran

--- shapefile_export.py
+++ shapefile_export.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 import os
+import json
 
 # Load shapefile
 gdf = gpd.read_file("ir/submaps/geoBoundaries-IRN-ADM1_simplified.shp")
@@ -8,14 +9,13 @@ gdf = gpd.read_file("ir/submaps/geoBoundaries-IRN-ADM1_simplified.shp")
 output_dir = "ir/submaps"
 os.makedirs(output_dir, exist_ok=True)
 
-#gdf.xs(30)['shapeISO'] = 'IR-07'
+with open("ir/submaps/map_translate.json", "r") as f:
+    states_translate = json.load(f)
 
 # Group by state name and export each as its own shapefile
 for state_name in gdf["shapeName"].unique():
-    if not gdf["shapeName"]:
-        gdf["shapeName"]='IR-07'
     state_gdf = gdf[gdf["shapeName"] == state_name]
-    filename = os.path.join(output_dir, f"{state_name.replace(' ', '_').replace('US-', '')}.shp")
+    filename = os.path.join(output_dir, f"{states_translate[state_name]}.shp")
     state_gdf.to_file(filename)
     print(f"Exported: {filename}")
 
