13c9d0a83132a95490ea8d40c6f6ec8058e79c28
cgrundman <christian.grundman@gmail.com>
2026-02-01 11:44:53 +0100

    update label plotting for de

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ from utils.file_utils import ensure_directory_exists
 
 
 # Set global variables, directories for map creation and site locations
-SCALE = 5
+SCALE = 4
 
 # US National Park Global Variables
 #PATH = "us"
 
 # Germany Global Variables
-#PATH = "de"
+PATH = "de"
 
 # Europe Global Variables
 #PATH = "eu"
 
 # Iran Global Variables
-PATH = "ir"
+#PATH = "ir"
 
 # Ensure output folders exist
 ensure_directory_exists("plots/temp")
