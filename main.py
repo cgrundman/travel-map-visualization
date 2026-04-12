81951c5a951505d5b4343f452f05d722e0da6268
cgrundman <christian.grundman@gmail.com>
2026-04-12 15:26:52 +0200

    update colors

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from utils.file_utils import ensure_directory_exists
 # Map Directory
 # US NP | Germany | Europe | Iran |
 #  "us" |    "de" |   "eu" | "ir" |
-PATH = "eu"
+PATH = "de"
 
 small_map = True
 small_gif = False
