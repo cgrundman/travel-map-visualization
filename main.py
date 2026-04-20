067287b5dba6c43fe732432a0f785f271a676a31
cgrundman <christian.grundman@gmail.com>
2026-04-20 09:43:55 +0200

    correct label locations

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from utils.file_utils import ensure_directory_exists
 # Map Directory
 # US NP | Germany | Europe | Iran |
 #  "us" |    "de" |   "eu" | "ir" |
-PATH = "eu"
+PATH = "ir"
 
-small_map = True
-small_gif = True
+small_map = False
+small_gif = False
 large_map = True
 
 # Ensure output folders exist
