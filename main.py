8f8d72e193b4f3fa7742f75aaeb7ae778d96bfdd
cgrundman <christian.grundman@gmail.com>
2026-04-24 17:54:58 +0200

    adjust label positions

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from utils.file_utils import ensure_directory_exists
 # Map Directory
 # US NP | Germany | Europe | Iran |
 #  "us" |    "de" |   "eu" | "ir" |
-PATH = "de"
+PATH = "ir"
 
-small_map = True
+small_map = False
 small_gif = False
-large_map = False
+large_map = True
 
 # Ensure output folders exist
 ensure_directory_exists("plots/temp")
