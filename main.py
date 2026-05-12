b117e28911d791b7407339973b98e7adf1ce6fa2
cgrundman <christian.grundman@gmail.com>
2026-05-12 07:58:07 +0200

    update maps

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from utils.file_utils import ensure_directory_exists
 # Map Directory
 # US NP | Germany | Europe | Iran |
 #  "us" |    "de" |   "eu" | "ir" |
-PATH = "de"
+PATH = "eu"
 
-small_map = True
-small_gif = True
+small_map = False
+small_gif = False
 large_map = True
 
 # Ensure output folders exist
